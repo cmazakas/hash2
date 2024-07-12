2729e92a3cde927f03ee87c6050cd6ddda04249f
Christian Mazakas <christian.mazakas@gmail.com>
2024-07-12 15:56:47 -0700

    update ripemd to use verbose expanded implementation

--- include/boost/hash2/ripemd.hpp
+++ include/boost/hash2/ripemd.hpp
@@ -47,31 +47,81 @@ private:
         state_[ 4 ] = 0xc3d2e1f0u;
     }
 
-    static BOOST_FORCEINLINE std::uint32_t f1(std::uint32_t x, std::uint32_t y, std::uint32_t z)
+    static BOOST_FORCEINLINE std::uint32_t F1( std::uint32_t x, std::uint32_t y, std::uint32_t z) { return x ^ y ^ z; }
+    static BOOST_FORCEINLINE std::uint32_t F2( std::uint32_t x, std::uint32_t y, std::uint32_t z) { return (x & y) | (~x & z); }
+    static BOOST_FORCEINLINE std::uint32_t F3( std::uint32_t x, std::uint32_t y, std::uint32_t z) { return (x | ~y) ^ z; }
+    static BOOST_FORCEINLINE std::uint32_t F4( std::uint32_t x, std::uint32_t y, std::uint32_t z) { return (x & z) | (y & ~z); }
+    static BOOST_FORCEINLINE std::uint32_t F5( std::uint32_t x, std::uint32_t y, std::uint32_t z) { return x ^ (y | ~z); }
+
+    static BOOST_FORCEINLINE void R1( std::uint32_t & a, std::uint32_t b, std::uint32_t & c, std::uint32_t d, std::uint32_t e, std::uint32_t x, std::uint32_t s )
     {
-        return (x ^ y ^ z);
+        a += F1(b, c, d) + x;
+        a = detail::rotl(a, s) + e;
+        c = detail::rotl(c, 10);
     }
 
-    static BOOST_FORCEINLINE std::uint32_t f2(std::uint32_t x, std::uint32_t y, std::uint32_t z)
+    static BOOST_FORCEINLINE void R2( std::uint32_t & a, std::uint32_t b, std::uint32_t & c, std::uint32_t d, std::uint32_t e, std::uint32_t x, std::uint32_t s )
     {
-        return (x & y) | (~x & z);
+        a += F2(b, c, d) + x + 0x5a827999u;
+        a = detail::rotl(a, s) + e;
+        c = detail::rotl(c, 10);
     }
 
-    static BOOST_FORCEINLINE std::uint32_t f3(std::uint32_t x, std::uint32_t y, std::uint32_t z)
+    static BOOST_FORCEINLINE void R3( std::uint32_t & a, std::uint32_t b, std::uint32_t & c, std::uint32_t d, std::uint32_t e, std::uint32_t x, std::uint32_t s )
     {
-        return (x | ~y) ^ z;
+        a += F3(b, c, d) + x + 0x6ed9eba1u;
+        a = detail::rotl(a, s) + e;
+        c = detail::rotl(c, 10);
     }
 
-    static BOOST_FORCEINLINE std::uint32_t f4(std::uint32_t x, std::uint32_t y, std::uint32_t z)
+    static BOOST_FORCEINLINE void R4( std::uint32_t & a, std::uint32_t b, std::uint32_t & c, std::uint32_t d, std::uint32_t e, std::uint32_t x, std::uint32_t s )
     {
-        return (x & z) | (y & ~z);
+        a += F4(b, c, d) + x + 0x8f1bbcdcu;
+        a = detail::rotl(a, s) + e;
+        c = detail::rotl(c, 10);
     }
 
-    static BOOST_FORCEINLINE std::uint32_t f5(std::uint32_t x, std::uint32_t y, std::uint32_t z)
+    static BOOST_FORCEINLINE void R5( std::uint32_t & a, std::uint32_t b, std::uint32_t & c, std::uint32_t d, std::uint32_t e, std::uint32_t x, std::uint32_t s )
     {
-        return x ^ (y | ~z);
+        a += F5(b, c, d) + x + 0xa953fd4eu;
+        a = detail::rotl(a, s) + e;
+        c = detail::rotl(c, 10);
     }
 
+    static BOOST_FORCEINLINE void RR1( std::uint32_t & a, std::uint32_t b, std::uint32_t & c, std::uint32_t d, std::uint32_t e, std::uint32_t x, std::uint32_t s )
+    {
+        a += F1(b, c, d) + x;
+        a = detail::rotl(a, s) + e;
+        c = detail::rotl(c, 10);
+    }
+
+    static BOOST_FORCEINLINE void RR2( std::uint32_t & a, std::uint32_t b, std::uint32_t & c, std::uint32_t d, std::uint32_t e, std::uint32_t x, std::uint32_t s )
+    {
+        a += F2(b, c, d) + x + 0x7a6d76e9u;
+        a = detail::rotl(a, s) + e;
+        c = detail::rotl(c, 10);
+    }
+
+    static BOOST_FORCEINLINE void RR3( std::uint32_t & a, std::uint32_t b, std::uint32_t & c, std::uint32_t d, std::uint32_t e, std::uint32_t x, std::uint32_t s )
+    {
+        a += F3(b, c, d) + x + 0x6d703ef3u;
+        a = detail::rotl(a, s) + e;
+        c = detail::rotl(c, 10);
+    }
+
+    static BOOST_FORCEINLINE void RR4( std::uint32_t & a, std::uint32_t b, std::uint32_t & c, std::uint32_t d, std::uint32_t e, std::uint32_t x, std::uint32_t s )
+    {
+        a += F4(b, c, d) + x + 0x5c4dd124u;
+        a = detail::rotl(a, s) + e;
+        c = detail::rotl(c, 10);
+    }
+
+    static BOOST_FORCEINLINE void RR5( std::uint32_t & a, std::uint32_t b, std::uint32_t & c, std::uint32_t d, std::uint32_t e, std::uint32_t x, std::uint32_t s )
+    {
+        a += F5(b, c, d) + x + 0x50a28be6u;
+        a = detail::rotl(a, s) + e;
+        c = detail::rotl(c, 10);
+    }
 
     void transform( unsigned char const block[ 64 ] )
     {
@@ -91,176 +141,197 @@ private:
 
         for( int i = 0; i < 16; ++i )
         {
-        X[ i ] = detail::read32le( block + i * 4 );
+            X[ i ] = detail::read32le( block + i * 4 );
         }
 
-        for( int i = 0; i < 16; ++i )
-        {
-        // round 1
+        // each function mutates a and c inputs so that we can easily rotate the arguments when expanding the evaluation
+        // of the core algorithm in the paper:
+        // T = rotl(A + f(j, B, C, D) + X[i][r[j]] + K[j], s)
+        // A = E
+        // E = D
+        // D = rotl(C, 10)
+        // C = B
+        // B = T
+        // mutating A in-place as T permits us to use it as B in the next call and so on and so forth
+        // mutating C in-place permits us to use it as D in later calls as well and so on and so forth
 
-        std::uint32_t K = 0x00000000u;
-        std::uint32_t r[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-        std::uint32_t s[16] = {11, 14, 15, 12, 5, 8, 7, 9, 11, 13, 14, 15, 6, 7, 9, 8};
+        //  A   B   C   D   E
+        R1(aa, bb, cc, dd, ee, X[ 0], 11);
+        R1(ee, aa, bb, cc, dd, X[ 1], 14);
+        R1(dd, ee, aa, bb, cc, X[ 2], 15);
+        R1(cc, dd, ee, aa, bb, X[ 3], 12);
+        R1(bb, cc, dd, ee, aa, X[ 4],  5);
+        R1(aa, bb, cc, dd, ee, X[ 5],  8);
+        R1(ee, aa, bb, cc, dd, X[ 6],  7);
+        R1(dd, ee, aa, bb, cc, X[ 7],  9);
+        R1(cc, dd, ee, aa, bb, X[ 8], 11);
+        R1(bb, cc, dd, ee, aa, X[ 9], 13);
+        R1(aa, bb, cc, dd, ee, X[10], 14);
+        R1(ee, aa, bb, cc, dd, X[11], 15);
+        R1(dd, ee, aa, bb, cc, X[12],  6);
+        R1(cc, dd, ee, aa, bb, X[13],  7);
+        R1(bb, cc, dd, ee, aa, X[14],  9);
+        R1(aa, bb, cc, dd, ee, X[15],  8);
 
-        auto T = detail::rotl(aa + f1(bb, cc, dd) + X[r[i]] + K, s[i]) + ee;
-        aa = ee;
-        ee = dd;
-        dd = detail::rotl(cc, 10);
-        cc = bb;
-        bb = T;
-        }
+        R2(ee, aa, bb, cc, dd, X[ 7],  7);
+        R2(dd, ee, aa, bb, cc, X[ 4],  6);
+        R2(cc, dd, ee, aa, bb, X[13],  8);
+        R2(bb, cc, dd, ee, aa, X[ 1], 13);
+        R2(aa, bb, cc, dd, ee, X[10], 11);
+        R2(ee, aa, bb, cc, dd, X[ 6],  9);
+        R2(dd, ee, aa, bb, cc, X[15],  7);
+        R2(cc, dd, ee, aa, bb, X[ 3], 15);
+        R2(bb, cc, dd, ee, aa, X[12],  7);
+        R2(aa, bb, cc, dd, ee, X[ 0], 12);
+        R2(ee, aa, bb, cc, dd, X[ 9], 15);
+        R2(dd, ee, aa, bb, cc, X[ 5],  9);
+        R2(cc, dd, ee, aa, bb, X[ 2], 11);
+        R2(bb, cc, dd, ee, aa, X[14],  7);
+        R2(aa, bb, cc, dd, ee, X[11], 13);
+        R2(ee, aa, bb, cc, dd, X[ 8], 12);
 
-        for( int i = 0; i < 16; ++i )
-        {
-        // round 2
+        R3(dd, ee, aa, bb, cc, X[ 3], 11);
+        R3(cc, dd, ee, aa, bb, X[10], 13);
+        R3(bb, cc, dd, ee, aa, X[14],  6);
+        R3(aa, bb, cc, dd, ee, X[ 4],  7);
+        R3(ee, aa, bb, cc, dd, X[ 9], 14);
+        R3(dd, ee, aa, bb, cc, X[15],  9);
+        R3(cc, dd, ee, aa, bb, X[ 8], 13);
+        R3(bb, cc, dd, ee, aa, X[ 1], 15);
+        R3(aa, bb, cc, dd, ee, X[ 2], 14);
+        R3(ee, aa, bb, cc, dd, X[ 7],  8);
+        R3(dd, ee, aa, bb, cc, X[ 0], 13);
+        R3(cc, dd, ee, aa, bb, X[ 6],  6);
+        R3(bb, cc, dd, ee, aa, X[13],  5);
+        R3(aa, bb, cc, dd, ee, X[11], 12);
+        R3(ee, aa, bb, cc, dd, X[ 5],  7);
+        R3(dd, ee, aa, bb, cc, X[12],  5);
 
-        std::uint32_t K = 0x5a827999u;
-        std::uint32_t r[16] = {7, 4, 13, 1, 10, 6, 15, 3, 12, 0, 9, 5, 2, 14, 11, 8};
-        std::uint32_t s[16] = {7, 6, 8, 13, 11, 9, 7, 15, 7, 12, 15, 9, 11, 7, 13, 12};
+        R4(cc, dd, ee, aa, bb, X[ 1], 11);
+        R4(bb, cc, dd, ee, aa, X[ 9], 12);
+        R4(aa, bb, cc, dd, ee, X[11], 14);
+        R4(ee, aa, bb, cc, dd, X[10], 15);
+        R4(dd, ee, aa, bb, cc, X[ 0], 14);
+        R4(cc, dd, ee, aa, bb, X[ 8], 15);
+        R4(bb, cc, dd, ee, aa, X[12],  9);
+        R4(aa, bb, cc, dd, ee, X[ 4],  8);
+        R4(ee, aa, bb, cc, dd, X[13],  9);
+        R4(dd, ee, aa, bb, cc, X[ 3], 14);
+        R4(cc, dd, ee, aa, bb, X[ 7],  5);
+        R4(bb, cc, dd, ee, aa, X[15],  6);
+        R4(aa, bb, cc, dd, ee, X[14],  8);
+        R4(ee, aa, bb, cc, dd, X[ 5],  6);
+        R4(dd, ee, aa, bb, cc, X[ 6],  5);
+        R4(cc, dd, ee, aa, bb, X[ 2], 12);
 
-        auto T = detail::rotl(aa + f2(bb, cc, dd) + X[r[i]] + K, s[i]) + ee;
-        aa = ee;
-        ee = dd;
-        dd = detail::rotl(cc, 10);
-        cc = bb;
-        bb = T;
-        }
+        R5(bb, cc, dd, ee, aa, X[ 4],  9);
+        R5(aa, bb, cc, dd, ee, X[ 0], 15);
+        R5(ee, aa, bb, cc, dd, X[ 5],  5);
+        R5(dd, ee, aa, bb, cc, X[ 9], 11);
+        R5(cc, dd, ee, aa, bb, X[ 7],  6);
+        R5(bb, cc, dd, ee, aa, X[12],  8);
+        R5(aa, bb, cc, dd, ee, X[ 2], 13);
+        R5(ee, aa, bb, cc, dd, X[10], 12);
+        R5(dd, ee, aa, bb, cc, X[14],  5);
+        R5(cc, dd, ee, aa, bb, X[ 1], 12);
+        R5(bb, cc, dd, ee, aa, X[ 3], 13);
+        R5(aa, bb, cc, dd, ee, X[ 8], 14);
+        R5(ee, aa, bb, cc, dd, X[11], 11);
+        R5(dd, ee, aa, bb, cc, X[ 6],  8);
+        R5(cc, dd, ee, aa, bb, X[15],  5);
+        R5(bb, cc, dd, ee, aa, X[13],  6);
 
-        for( int i = 0; i < 16; ++i )
-        {
-        // round 3
+        RR5(aaa, bbb, ccc, ddd, eee, X[ 5],  8);
+        RR5(eee, aaa, bbb, ccc, ddd, X[14],  9);
+        RR5(ddd, eee, aaa, bbb, ccc, X[ 7],  9);
+        RR5(ccc, ddd, eee, aaa, bbb, X[ 0], 11);
+        RR5(bbb, ccc, ddd, eee, aaa, X[ 9], 13);
+        RR5(aaa, bbb, ccc, ddd, eee, X[ 2], 15);
+        RR5(eee, aaa, bbb, ccc, ddd, X[11], 15);
+        RR5(ddd, eee, aaa, bbb, ccc, X[ 4],  5);
+        RR5(ccc, ddd, eee, aaa, bbb, X[13],  7);
+        RR5(bbb, ccc, ddd, eee, aaa, X[ 6],  7);
+        RR5(aaa, bbb, ccc, ddd, eee, X[15],  8);
+        RR5(eee, aaa, bbb, ccc, ddd, X[ 8], 11);
+        RR5(ddd, eee, aaa, bbb, ccc, X[ 1], 14);
+        RR5(ccc, ddd, eee, aaa, bbb, X[10], 14);
+        RR5(bbb, ccc, ddd, eee, aaa, X[ 3], 12);
+        RR5(aaa, bbb, ccc, ddd, eee, X[12],  6);
 
-        std::uint32_t K = 0x6ed9eba1u;
-        std::uint32_t r[16] = {3, 10, 14, 4, 9, 15, 8, 1, 2, 7, 0, 6, 13, 11, 5, 12};
-        std::uint32_t s[16] = {11, 13, 6, 7, 14, 9, 13, 15, 14, 8, 13, 6, 5, 12, 7, 5};
+        RR4(eee, aaa, bbb, ccc, ddd, X[ 6],  9);
+        RR4(ddd, eee, aaa, bbb, ccc, X[11], 13);
+        RR4(ccc, ddd, eee, aaa, bbb, X[ 3], 15);
+        RR4(bbb, ccc, ddd, eee, aaa, X[ 7],  7);
+        RR4(aaa, bbb, ccc, ddd, eee, X[ 0], 12);
+        RR4(eee, aaa, bbb, ccc, ddd, X[13],  8);
+        RR4(ddd, eee, aaa, bbb, ccc, X[ 5],  9);
+        RR4(ccc, ddd, eee, aaa, bbb, X[10], 11);
+        RR4(bbb, ccc, ddd, eee, aaa, X[14],  7);
+        RR4(aaa, bbb, ccc, ddd, eee, X[15],  7);
+        RR4(eee, aaa, bbb, ccc, ddd, X[ 8], 12);
+        RR4(ddd, eee, aaa, bbb, ccc, X[12],  7);
+        RR4(ccc, ddd, eee, aaa, bbb, X[ 4],  6);
+        RR4(bbb, ccc, ddd, eee, aaa, X[ 9], 15);
+        RR4(aaa, bbb, ccc, ddd, eee, X[ 1], 13);
+        RR4(eee, aaa, bbb, ccc, ddd, X[ 2], 11);
 
-        auto T = detail::rotl(aa + f3(bb, cc, dd) + X[r[i]] + K, s[i]) + ee;
-        aa = ee;
-        ee = dd;
-        dd = detail::rotl(cc, 10);
-        cc = bb;
-        bb = T;
-        }
+        RR3(ddd, eee, aaa, bbb, ccc, X[15],  9);
+        RR3(ccc, ddd, eee, aaa, bbb, X[ 5],  7);
+        RR3(bbb, ccc, ddd, eee, aaa, X[ 1], 15);
+        RR3(aaa, bbb, ccc, ddd, eee, X[ 3], 11);
+        RR3(eee, aaa, bbb, ccc, ddd, X[ 7],  8);
+        RR3(ddd, eee, aaa, bbb, ccc, X[14],  6);
+        RR3(ccc, ddd, eee, aaa, bbb, X[ 6],  6);
+        RR3(bbb, ccc, ddd, eee, aaa, X[ 9], 14);
+        RR3(aaa, bbb, ccc, ddd, eee, X[11], 12);
+        RR3(eee, aaa, bbb, ccc, ddd, X[ 8], 13);
+        RR3(ddd, eee, aaa, bbb, ccc, X[12],  5);
+        RR3(ccc, ddd, eee, aaa, bbb, X[ 2], 14);
+        RR3(bbb, ccc, ddd, eee, aaa, X[10], 13);
+        RR3(aaa, bbb, ccc, ddd, eee, X[ 0], 13);
+        RR3(eee, aaa, bbb, ccc, ddd, X[ 4],  7);
+        RR3(ddd, eee, aaa, bbb, ccc, X[13],  5);
 
-        for( int i = 0; i < 16; ++i )
-        {
-        // round 4
+        RR2(ccc, ddd, eee, aaa, bbb, X[ 8], 15);
+        RR2(bbb, ccc, ddd, eee, aaa, X[ 6],  5);
+        RR2(aaa, bbb, ccc, ddd, eee, X[ 4],  8);
+        RR2(eee, aaa, bbb, ccc, ddd, X[ 1], 11);
+        RR2(ddd, eee, aaa, bbb, ccc, X[ 3], 14);
+        RR2(ccc, ddd, eee, aaa, bbb, X[11], 14);
+        RR2(bbb, ccc, ddd, eee, aaa, X[15],  6);
+        RR2(aaa, bbb, ccc, ddd, eee, X[ 0], 14);
+        RR2(eee, aaa, bbb, ccc, ddd, X[ 5],  6);
+        RR2(ddd, eee, aaa, bbb, ccc, X[12],  9);
+        RR2(ccc, ddd, eee, aaa, bbb, X[ 2], 12);
+        RR2(bbb, ccc, ddd, eee, aaa, X[13],  9);
+        RR2(aaa, bbb, ccc, ddd, eee, X[ 9], 12);
+        RR2(eee, aaa, bbb, ccc, ddd, X[ 7],  5);
+        RR2(ddd, eee, aaa, bbb, ccc, X[10], 15);
+        RR2(ccc, ddd, eee, aaa, bbb, X[14],  8);
 
-        std::uint32_t K = 0x8f1bbcdcu;
-        std::uint32_t r[16] = {1, 9, 11, 10, 0, 8, 12, 4, 13, 3, 7, 15, 14, 5, 6, 2};
-        std::uint32_t s[16] = {11, 12, 14, 15, 14, 15, 9, 8, 9, 14, 5, 6, 8, 6, 5, 12};
+        RR1(bbb, ccc, ddd, eee, aaa, X[12] ,  8);
+        RR1(aaa, bbb, ccc, ddd, eee, X[15] ,  5);
+        RR1(eee, aaa, bbb, ccc, ddd, X[10] , 12);
+        RR1(ddd, eee, aaa, bbb, ccc, X[ 4] ,  9);
+        RR1(ccc, ddd, eee, aaa, bbb, X[ 1] , 12);
+        RR1(bbb, ccc, ddd, eee, aaa, X[ 5] ,  5);
+        RR1(aaa, bbb, ccc, ddd, eee, X[ 8] , 14);
+        RR1(eee, aaa, bbb, ccc, ddd, X[ 7] ,  6);
+        RR1(ddd, eee, aaa, bbb, ccc, X[ 6] ,  8);
+        RR1(ccc, ddd, eee, aaa, bbb, X[ 2] , 13);
+        RR1(bbb, ccc, ddd, eee, aaa, X[13] ,  6);
+        RR1(aaa, bbb, ccc, ddd, eee, X[14] ,  5);
+        RR1(eee, aaa, bbb, ccc, ddd, X[ 0] , 15);
+        RR1(ddd, eee, aaa, bbb, ccc, X[ 3] , 13);
+        RR1(ccc, ddd, eee, aaa, bbb, X[ 9] , 11);
+        RR1(bbb, ccc, ddd, eee, aaa, X[11] , 11);
 
-        auto T = detail::rotl(aa + f4(bb, cc, dd) + X[r[i]] + K, s[i]) + ee;
-        aa = ee;
-        ee = dd;
-        dd = detail::rotl(cc, 10);
-        cc = bb;
-        bb = T;
-        }
-
-        for( int i = 0; i < 16; ++i )
-        {
-        // round 5
-
-        std::uint32_t K = 0xa953fd4eu;
-        std::uint32_t r[16] = {4, 0, 5, 9, 7, 12, 2, 10, 14, 1, 3, 8, 11, 6, 15, 13};
-        std::uint32_t s[16] = {9, 15, 5, 11, 6, 8, 13, 12, 5, 12, 13, 14, 11, 8, 5, 6};
-
-        auto T = detail::rotl(aa + f5(bb, cc, dd) + X[r[i]] + K, s[i]) + ee;
-        aa = ee;
-        ee = dd;
-        dd = detail::rotl(cc, 10);
-        cc = bb;
-        bb = T;
-        }
-
-        for( int i = 0; i < 16; ++i )
-        {
-        // parallel round 1
-
-        std::uint32_t K = 0x50a28be6u;
-        std::uint32_t r[16] = {5, 14, 7, 0, 9, 2, 11, 4, 13, 6, 15, 8, 1, 10, 3, 12};
-        std::uint32_t s[16] = {8, 9, 9, 11, 13, 15, 15, 5, 7, 7, 8, 11, 14, 14, 12, 6};
-
-        auto T = detail::rotl(aaa + f5(bbb, ccc, ddd) + X[r[i]] + K, s[i]) + eee;
-        aaa = eee;
-        eee = ddd;
-        ddd = detail::rotl(ccc, 10);
-        ccc = bbb;
-        bbb = T;
-        }
-
-        for( int i = 0; i < 16; ++i )
-        {
-        // parallel round 2
-
-        std::uint32_t K = 0x5c4dd124u;
-        std::uint32_t r[16] = {6, 11, 3, 7, 0, 13, 5, 10, 14, 15, 8, 12, 4, 9, 1, 2};
-        std::uint32_t s[16] = {9, 13, 15, 7, 12, 8, 9, 11, 7, 7, 12, 7, 6, 15, 13, 11};
-
-        auto T = detail::rotl(aaa + f4(bbb, ccc, ddd) + X[r[i]] + K, s[i]) + eee;
-        aaa = eee;
-        eee = ddd;
-        ddd = detail::rotl(ccc, 10);
-        ccc = bbb;
-        bbb = T;
-        }
-
-        for( int i = 0; i < 16; ++i )
-        {
-        // parallel round 3
-
-        std::uint32_t K = 0x6d703ef3u;
-        std::uint32_t r[16] = {15, 5, 1, 3, 7, 14, 6, 9, 11, 8, 12, 2, 10, 0, 4, 13};
-        std::uint32_t s[16] = {9, 7, 15, 11, 8, 6, 6, 14, 12, 13, 5, 14, 13, 13, 7, 5};
-
-        auto T = detail::rotl(aaa + f3(bbb, ccc, ddd) + X[r[i]] + K, s[i]) + eee;
-        aaa = eee;
-        eee = ddd;
-        ddd = detail::rotl(ccc, 10);
-        ccc = bbb;
-        bbb = T;
-        }
-
-        for( int i = 0; i < 16; ++i )
-        {
-        // parallel round 4
-
-        std::uint32_t K = 0x7a6d76e9u;
-        std::uint32_t r[16] = {8, 6, 4, 1, 3, 11, 15, 0, 5, 12, 2, 13, 9, 7, 10, 14};
-        std::uint32_t s[16] = {15, 5, 8, 11, 14, 14, 6, 14, 6, 9, 12, 9, 12, 5, 15, 8};
-
-        auto T = detail::rotl(aaa + f2(bbb, ccc, ddd) + X[r[i]] + K, s[i]) + eee;
-        aaa = eee;
-        eee = ddd;
-        ddd = detail::rotl(ccc, 10);
-        ccc = bbb;
-        bbb = T;
-        }
-
-        for( int i = 0; i < 16; ++i )
-        {
-        // parallel round 5
-
-        std::uint32_t K = 0x00000000u;
-        std::uint32_t r[16] = {12, 15, 10, 4, 1, 5, 8, 7, 6, 2, 13, 14, 0, 3, 9, 11};
-        std::uint32_t s[16] = {8, 5, 12, 9, 12, 5, 14, 6, 8, 13, 6, 5, 15, 13, 11, 11};
-
-        auto T = detail::rotl(aaa + f1(bbb, ccc, ddd) + X[r[i]] + K, s[i]) + eee;
-        aaa = eee;
-        eee = ddd;
-        ddd = detail::rotl(ccc, 10);
-        ccc = bbb;
-        bbb = T;
-        }
-
-        auto T = state_[ 1 ] + cc + ddd;
-
+        ddd += cc + state_[ 1 ];
         state_[ 1 ] = state_[ 2 ] + dd + eee;
         state_[ 2 ] = state_[ 3 ] + ee + aaa;
         state_[ 3 ] = state_[ 4 ] + aa + bbb;
         state_[ 4 ] = state_[ 0 ] + bb + ccc;
-        state_[ 0 ] = T;
+        state_[ 0 ] = ddd;
     }
 
 public:
