bde35f5c36437052450f5f2278d261375daf29ad
Christian Mazakas <christian.mazakas@gmail.com>
2024-07-15 09:20:13 -0700

    rearrange ripemd operations for performance gains

--- include/boost/hash2/ripemd.hpp
+++ include/boost/hash2/ripemd.hpp
@@ -90,14 +90,14 @@ private:
 
     static BOOST_FORCEINLINE void RR1( std::uint32_t & a, std::uint32_t b, std::uint32_t & c, std::uint32_t d, std::uint32_t e, std::uint32_t x, std::uint32_t s )
     {
-        a += F1(b, c, d) + x;
+        a += F5(b, c, d) + x + 0x50a28be6u;
         a = detail::rotl(a, s) + e;
         c = detail::rotl(c, 10);
     }
 
     static BOOST_FORCEINLINE void RR2( std::uint32_t & a, std::uint32_t b, std::uint32_t & c, std::uint32_t d, std::uint32_t e, std::uint32_t x, std::uint32_t s )
     {
-        a += F2(b, c, d) + x + 0x7a6d76e9u;
+        a += F4(b, c, d) + x + 0x5c4dd124u;
         a = detail::rotl(a, s) + e;
         c = detail::rotl(c, 10);
     }
@@ -111,14 +111,14 @@ private:
 
     static BOOST_FORCEINLINE void RR4( std::uint32_t & a, std::uint32_t b, std::uint32_t & c, std::uint32_t d, std::uint32_t e, std::uint32_t x, std::uint32_t s )
     {
-        a += F4(b, c, d) + x + 0x5c4dd124u;
+        a += F2(b, c, d) + x + 0x7a6d76e9u;
         a = detail::rotl(a, s) + e;
         c = detail::rotl(c, 10);
     }
 
     static BOOST_FORCEINLINE void RR5( std::uint32_t & a, std::uint32_t b, std::uint32_t & c, std::uint32_t d, std::uint32_t e, std::uint32_t x, std::uint32_t s )
     {
-        a += F5(b, c, d) + x + 0x50a28be6u;
+        a += F1(b, c, d) + x;
         a = detail::rotl(a, s) + e;
         c = detail::rotl(c, 10);
     }
@@ -173,6 +173,23 @@ private:
         R1(bb, cc, dd, ee, aa, X[14],  9);
         R1(aa, bb, cc, dd, ee, X[15],  8);
 
+        RR1(aaa, bbb, ccc, ddd, eee, X[ 5],  8);
+        RR1(eee, aaa, bbb, ccc, ddd, X[14],  9);
+        RR1(ddd, eee, aaa, bbb, ccc, X[ 7],  9);
+        RR1(ccc, ddd, eee, aaa, bbb, X[ 0], 11);
+        RR1(bbb, ccc, ddd, eee, aaa, X[ 9], 13);
+        RR1(aaa, bbb, ccc, ddd, eee, X[ 2], 15);
+        RR1(eee, aaa, bbb, ccc, ddd, X[11], 15);
+        RR1(ddd, eee, aaa, bbb, ccc, X[ 4],  5);
+        RR1(ccc, ddd, eee, aaa, bbb, X[13],  7);
+        RR1(bbb, ccc, ddd, eee, aaa, X[ 6],  7);
+        RR1(aaa, bbb, ccc, ddd, eee, X[15],  8);
+        RR1(eee, aaa, bbb, ccc, ddd, X[ 8], 11);
+        RR1(ddd, eee, aaa, bbb, ccc, X[ 1], 14);
+        RR1(ccc, ddd, eee, aaa, bbb, X[10], 14);
+        RR1(bbb, ccc, ddd, eee, aaa, X[ 3], 12);
+        RR1(aaa, bbb, ccc, ddd, eee, X[12],  6);
+
         R2(ee, aa, bb, cc, dd, X[ 7],  7);
         R2(dd, ee, aa, bb, cc, X[ 4],  6);
         R2(cc, dd, ee, aa, bb, X[13],  8);
@@ -189,6 +206,23 @@ private:
         R2(bb, cc, dd, ee, aa, X[14],  7);
         R2(aa, bb, cc, dd, ee, X[11], 13);
         R2(ee, aa, bb, cc, dd, X[ 8], 12);
+
+        RR2(eee, aaa, bbb, ccc, ddd, X[ 6],  9);
+        RR2(ddd, eee, aaa, bbb, ccc, X[11], 13);
+        RR2(ccc, ddd, eee, aaa, bbb, X[ 3], 15);
+        RR2(bbb, ccc, ddd, eee, aaa, X[ 7],  7);
+        RR2(aaa, bbb, ccc, ddd, eee, X[ 0], 12);
+        RR2(eee, aaa, bbb, ccc, ddd, X[13],  8);
+        RR2(ddd, eee, aaa, bbb, ccc, X[ 5],  9);
+        RR2(ccc, ddd, eee, aaa, bbb, X[10], 11);
+        RR2(bbb, ccc, ddd, eee, aaa, X[14],  7);
+        RR2(aaa, bbb, ccc, ddd, eee, X[15],  7);
+        RR2(eee, aaa, bbb, ccc, ddd, X[ 8], 12);
+        RR2(ddd, eee, aaa, bbb, ccc, X[12],  7);
+        RR2(ccc, ddd, eee, aaa, bbb, X[ 4],  6);
+        RR2(bbb, ccc, ddd, eee, aaa, X[ 9], 15);
+        RR2(aaa, bbb, ccc, ddd, eee, X[ 1], 13);
+        RR2(eee, aaa, bbb, ccc, ddd, X[ 2], 11);
 
         R3(dd, ee, aa, bb, cc, X[ 3], 11);
         R3(cc, dd, ee, aa, bb, X[10], 13);
@@ -207,6 +241,23 @@ private:
         R3(ee, aa, bb, cc, dd, X[ 5],  7);
         R3(dd, ee, aa, bb, cc, X[12],  5);
 
+        RR3(ddd, eee, aaa, bbb, ccc, X[15],  9);
+        RR3(ccc, ddd, eee, aaa, bbb, X[ 5],  7);
+        RR3(bbb, ccc, ddd, eee, aaa, X[ 1], 15);
+        RR3(aaa, bbb, ccc, ddd, eee, X[ 3], 11);
+        RR3(eee, aaa, bbb, ccc, ddd, X[ 7],  8);
+        RR3(ddd, eee, aaa, bbb, ccc, X[14],  6);
+        RR3(ccc, ddd, eee, aaa, bbb, X[ 6],  6);
+        RR3(bbb, ccc, ddd, eee, aaa, X[ 9], 14);
+        RR3(aaa, bbb, ccc, ddd, eee, X[11], 12);
+        RR3(eee, aaa, bbb, ccc, ddd, X[ 8], 13);
+        RR3(ddd, eee, aaa, bbb, ccc, X[12],  5);
+        RR3(ccc, ddd, eee, aaa, bbb, X[ 2], 14);
+        RR3(bbb, ccc, ddd, eee, aaa, X[10], 13);
+        RR3(aaa, bbb, ccc, ddd, eee, X[ 0], 13);
+        RR3(eee, aaa, bbb, ccc, ddd, X[ 4],  7);
+        RR3(ddd, eee, aaa, bbb, ccc, X[13],  5);
+
         R4(cc, dd, ee, aa, bb, X[ 1], 11);
         R4(bb, cc, dd, ee, aa, X[ 9], 12);
         R4(aa, bb, cc, dd, ee, X[11], 14);
@@ -223,6 +274,23 @@ private:
         R4(ee, aa, bb, cc, dd, X[ 5],  6);
         R4(dd, ee, aa, bb, cc, X[ 6],  5);
         R4(cc, dd, ee, aa, bb, X[ 2], 12);
+
+        RR4(ccc, ddd, eee, aaa, bbb, X[ 8], 15);
+        RR4(bbb, ccc, ddd, eee, aaa, X[ 6],  5);
+        RR4(aaa, bbb, ccc, ddd, eee, X[ 4],  8);
+        RR4(eee, aaa, bbb, ccc, ddd, X[ 1], 11);
+        RR4(ddd, eee, aaa, bbb, ccc, X[ 3], 14);
+        RR4(ccc, ddd, eee, aaa, bbb, X[11], 14);
+        RR4(bbb, ccc, ddd, eee, aaa, X[15],  6);
+        RR4(aaa, bbb, ccc, ddd, eee, X[ 0], 14);
+        RR4(eee, aaa, bbb, ccc, ddd, X[ 5],  6);
+        RR4(ddd, eee, aaa, bbb, ccc, X[12],  9);
+        RR4(ccc, ddd, eee, aaa, bbb, X[ 2], 12);
+        RR4(bbb, ccc, ddd, eee, aaa, X[13],  9);
+        RR4(aaa, bbb, ccc, ddd, eee, X[ 9], 12);
+        RR4(eee, aaa, bbb, ccc, ddd, X[ 7],  5);
+        RR4(ddd, eee, aaa, bbb, ccc, X[10], 15);
+        RR4(ccc, ddd, eee, aaa, bbb, X[14],  8);
 
         R5(bb, cc, dd, ee, aa, X[ 4],  9);
         R5(aa, bb, cc, dd, ee, X[ 0], 15);
@@ -241,90 +309,22 @@ private:
         R5(cc, dd, ee, aa, bb, X[15],  5);
         R5(bb, cc, dd, ee, aa, X[13],  6);
 
-        RR5(aaa, bbb, ccc, ddd, eee, X[ 5],  8);
-        RR5(eee, aaa, bbb, ccc, ddd, X[14],  9);
-        RR5(ddd, eee, aaa, bbb, ccc, X[ 7],  9);
-        RR5(ccc, ddd, eee, aaa, bbb, X[ 0], 11);
-        RR5(bbb, ccc, ddd, eee, aaa, X[ 9], 13);
-        RR5(aaa, bbb, ccc, ddd, eee, X[ 2], 15);
-        RR5(eee, aaa, bbb, ccc, ddd, X[11], 15);
-        RR5(ddd, eee, aaa, bbb, ccc, X[ 4],  5);
-        RR5(ccc, ddd, eee, aaa, bbb, X[13],  7);
-        RR5(bbb, ccc, ddd, eee, aaa, X[ 6],  7);
-        RR5(aaa, bbb, ccc, ddd, eee, X[15],  8);
-        RR5(eee, aaa, bbb, ccc, ddd, X[ 8], 11);
-        RR5(ddd, eee, aaa, bbb, ccc, X[ 1], 14);
-        RR5(ccc, ddd, eee, aaa, bbb, X[10], 14);
-        RR5(bbb, ccc, ddd, eee, aaa, X[ 3], 12);
-        RR5(aaa, bbb, ccc, ddd, eee, X[12],  6);
-
-        RR4(eee, aaa, bbb, ccc, ddd, X[ 6],  9);
-        RR4(ddd, eee, aaa, bbb, ccc, X[11], 13);
-        RR4(ccc, ddd, eee, aaa, bbb, X[ 3], 15);
-        RR4(bbb, ccc, ddd, eee, aaa, X[ 7],  7);
-        RR4(aaa, bbb, ccc, ddd, eee, X[ 0], 12);
-        RR4(eee, aaa, bbb, ccc, ddd, X[13],  8);
-        RR4(ddd, eee, aaa, bbb, ccc, X[ 5],  9);
-        RR4(ccc, ddd, eee, aaa, bbb, X[10], 11);
-        RR4(bbb, ccc, ddd, eee, aaa, X[14],  7);
-        RR4(aaa, bbb, ccc, ddd, eee, X[15],  7);
-        RR4(eee, aaa, bbb, ccc, ddd, X[ 8], 12);
-        RR4(ddd, eee, aaa, bbb, ccc, X[12],  7);
-        RR4(ccc, ddd, eee, aaa, bbb, X[ 4],  6);
-        RR4(bbb, ccc, ddd, eee, aaa, X[ 9], 15);
-        RR4(aaa, bbb, ccc, ddd, eee, X[ 1], 13);
-        RR4(eee, aaa, bbb, ccc, ddd, X[ 2], 11);
-
-        RR3(ddd, eee, aaa, bbb, ccc, X[15],  9);
-        RR3(ccc, ddd, eee, aaa, bbb, X[ 5],  7);
-        RR3(bbb, ccc, ddd, eee, aaa, X[ 1], 15);
-        RR3(aaa, bbb, ccc, ddd, eee, X[ 3], 11);
-        RR3(eee, aaa, bbb, ccc, ddd, X[ 7],  8);
-        RR3(ddd, eee, aaa, bbb, ccc, X[14],  6);
-        RR3(ccc, ddd, eee, aaa, bbb, X[ 6],  6);
-        RR3(bbb, ccc, ddd, eee, aaa, X[ 9], 14);
-        RR3(aaa, bbb, ccc, ddd, eee, X[11], 12);
-        RR3(eee, aaa, bbb, ccc, ddd, X[ 8], 13);
-        RR3(ddd, eee, aaa, bbb, ccc, X[12],  5);
-        RR3(ccc, ddd, eee, aaa, bbb, X[ 2], 14);
-        RR3(bbb, ccc, ddd, eee, aaa, X[10], 13);
-        RR3(aaa, bbb, ccc, ddd, eee, X[ 0], 13);
-        RR3(eee, aaa, bbb, ccc, ddd, X[ 4],  7);
-        RR3(ddd, eee, aaa, bbb, ccc, X[13],  5);
-
-        RR2(ccc, ddd, eee, aaa, bbb, X[ 8], 15);
-        RR2(bbb, ccc, ddd, eee, aaa, X[ 6],  5);
-        RR2(aaa, bbb, ccc, ddd, eee, X[ 4],  8);
-        RR2(eee, aaa, bbb, ccc, ddd, X[ 1], 11);
-        RR2(ddd, eee, aaa, bbb, ccc, X[ 3], 14);
-        RR2(ccc, ddd, eee, aaa, bbb, X[11], 14);
-        RR2(bbb, ccc, ddd, eee, aaa, X[15],  6);
-        RR2(aaa, bbb, ccc, ddd, eee, X[ 0], 14);
-        RR2(eee, aaa, bbb, ccc, ddd, X[ 5],  6);
-        RR2(ddd, eee, aaa, bbb, ccc, X[12],  9);
-        RR2(ccc, ddd, eee, aaa, bbb, X[ 2], 12);
-        RR2(bbb, ccc, ddd, eee, aaa, X[13],  9);
-        RR2(aaa, bbb, ccc, ddd, eee, X[ 9], 12);
-        RR2(eee, aaa, bbb, ccc, ddd, X[ 7],  5);
-        RR2(ddd, eee, aaa, bbb, ccc, X[10], 15);
-        RR2(ccc, ddd, eee, aaa, bbb, X[14],  8);
-
-        RR1(bbb, ccc, ddd, eee, aaa, X[12] ,  8);
-        RR1(aaa, bbb, ccc, ddd, eee, X[15] ,  5);
-        RR1(eee, aaa, bbb, ccc, ddd, X[10] , 12);
-        RR1(ddd, eee, aaa, bbb, ccc, X[ 4] ,  9);
-        RR1(ccc, ddd, eee, aaa, bbb, X[ 1] , 12);
-        RR1(bbb, ccc, ddd, eee, aaa, X[ 5] ,  5);
-        RR1(aaa, bbb, ccc, ddd, eee, X[ 8] , 14);
-        RR1(eee, aaa, bbb, ccc, ddd, X[ 7] ,  6);
-        RR1(ddd, eee, aaa, bbb, ccc, X[ 6] ,  8);
-        RR1(ccc, ddd, eee, aaa, bbb, X[ 2] , 13);
-        RR1(bbb, ccc, ddd, eee, aaa, X[13] ,  6);
-        RR1(aaa, bbb, ccc, ddd, eee, X[14] ,  5);
-        RR1(eee, aaa, bbb, ccc, ddd, X[ 0] , 15);
-        RR1(ddd, eee, aaa, bbb, ccc, X[ 3] , 13);
-        RR1(ccc, ddd, eee, aaa, bbb, X[ 9] , 11);
-        RR1(bbb, ccc, ddd, eee, aaa, X[11] , 11);
+        RR5(bbb, ccc, ddd, eee, aaa, X[12] ,  8);
+        RR5(aaa, bbb, ccc, ddd, eee, X[15] ,  5);
+        RR5(eee, aaa, bbb, ccc, ddd, X[10] , 12);
+        RR5(ddd, eee, aaa, bbb, ccc, X[ 4] ,  9);
+        RR5(ccc, ddd, eee, aaa, bbb, X[ 1] , 12);
+        RR5(bbb, ccc, ddd, eee, aaa, X[ 5] ,  5);
+        RR5(aaa, bbb, ccc, ddd, eee, X[ 8] , 14);
+        RR5(eee, aaa, bbb, ccc, ddd, X[ 7] ,  6);
+        RR5(ddd, eee, aaa, bbb, ccc, X[ 6] ,  8);
+        RR5(ccc, ddd, eee, aaa, bbb, X[ 2] , 13);
+        RR5(bbb, ccc, ddd, eee, aaa, X[13] ,  6);
+        RR5(aaa, bbb, ccc, ddd, eee, X[14] ,  5);
+        RR5(eee, aaa, bbb, ccc, ddd, X[ 0] , 15);
+        RR5(ddd, eee, aaa, bbb, ccc, X[ 3] , 13);
+        RR5(ccc, ddd, eee, aaa, bbb, X[ 9] , 11);
+        RR5(bbb, ccc, ddd, eee, aaa, X[11] , 11);
 
         ddd += cc + state_[ 1 ];
         state_[ 1 ] = state_[ 2 ] + dd + eee;
